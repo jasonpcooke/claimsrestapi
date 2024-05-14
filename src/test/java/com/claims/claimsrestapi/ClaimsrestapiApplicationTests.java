40c8b26ae78203e167e30bd49c262faf69b20644
Jason Cooke <jasonpcooke@gmail.com>
2024-05-14 11:31:02 +0100

    Initialisation of project using spring boot starter website and dependencies.

---
+++ src/test/java/com/claims/claimsrestapi/ClaimsrestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claims.claimsrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimsrestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
